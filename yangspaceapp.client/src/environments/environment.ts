bc348ba31ff64e5a437512c3a9baaf0ffa4cef3f
Yordan <yordan.valkov@gmail.com>
2024-12-09 15:27:20 +0200

    Fix the url issues

--- yangspaceapp.client/src/environments/environment.ts
+++ yangspaceapp.client/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5290/api', // your API endpoint
-  imageUrl: 'http://localhost:5290', // your Images endpoint
+  production: true,
+  apiUrl: 'https://yangspace.azurewebsites.net/api', // your API endpoint
+  imageUrl: 'https://yangspace.azurewebsites.net', // your Images endpoint
   tokenKey: 'app_token',
   usernameKey: 'app_username',
   userRoleName: 'app_role'
