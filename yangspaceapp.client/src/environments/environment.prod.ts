7f741542c01c8d14a312ced0dff8815603c34042
Yordan <yordan.valkov@gmail.com>
2024-12-09 13:49:24 +0200

    Add project files.

---
+++ yangspaceapp.client/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://yangspace.gleeze.com', // your API endpoint
+  imageUrl: 'https://yangspace.gleeze.com', // your Images endpoint
+  tokenKey: 'app_token',
+  usernameKey: 'app_username',
+  userRoleName: 'app_role'
+};
